15be66b7048cee4c0453b8231ae08d6d6d3ff95a
siblife54-dot <siblife54@gmail.com>
2026-03-30 12:19:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ window.APP_CONFIG = {
   courseId: "course_sova",
 
   // Публичный CSV URL из Google Sheets
-  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS9-CZbaqUwGjDG9zY2eRzaW5jKUrkeW7JBklA1E-CH3lwiTQ2g6BIsDCALmVOvV8AhTEqoTNwmkb4t/export?format=csv",
+  googleSheetCsvUrl: "https://docs.google.com/spreadsheets/d/1GwvdAt1YkEdimh4Y4DqyA8SGsSDyFMmhhGy3bfraWpE/export?format=csv",
 
   // Локальный демо-файл
   sampleCsvPath: "./sample-sheet.csv"
